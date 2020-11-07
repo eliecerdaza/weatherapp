52e0c31884b03edda8585165654b73009b258e0a
Eliecer Daza <dazaeliecer@gmail.com>
2020-11-07 12:03:05 -0500

    code improvements, better on_get responsabilities

--- app/views.py
+++ app/views.py
@@ -15,19 +15,11 @@ class WeatherView:
         day = req.get_param('day') or '-1'
         try:
             weather_service = OpenWeatherService()
-            if cache.get(f'{city}_{country}'):
-                weather_response = cache.get(f'{city}_{country}')
-            else:
-                weather_response = self._get_weather(weather_service, city, country)
-                cache.set(f'{city}_{country}', weather_response)
+            weather_response = self._get_weather(weather_service, city, country)
             weather_data = weather_response.json()
             if day != '-1':
                 coord = weather_data.get('coord')
-                if cache.get(f'{coord["lat"]}_{coord["lon"]}_{day}'):
-                    weather_data["forecast"] = cache.get(f'{coord["lat"]}_{coord["lon"]}_{day}')
-                else:
-                    weather_data["forecast"] = self._get_forecast(weather_service, coord, int(day))
-                    cache.set(f'{coord["lat"]}_{coord["lon"]}_{day}', weather_data["forecast"])
+                weather_data["forecast"] = self._get_forecast(weather_service, coord, int(day))
             weather_adapter = WeatherAdapter()
             resp.status = falcon.HTTP_200
             resp.content_type = 'application/json'
@@ -41,12 +33,19 @@ class WeatherView:
             resp.body = 'Error on weather request'
 
     def _get_weather(self, ws, city, country):
-        return ws.get_by_city_country(f'{city},{country}')
+        if cache.get(f'{city}_{country}'):
+            return cache.get(f'{city}_{country}')
+        weather_response = ws.get_by_city_country(f'{city},{country}')
+        cache.set(f'{city}_{country}', weather_response)
+        return weather_response
 
     def _get_forecast(self, ws, coord:dict, day:int) -> dict:
+        if cache.get(f'{coord["lat"]}_{coord["lon"]}_{day}'):
+            return cache.get(f'{coord["lat"]}_{coord["lon"]}_{day}')
         forecast_response = ws.get_forecast(coord['lat'], coord['lon'])
         forecast_data = forecast_response.json()
         forecast = forecast_data.get('daily')[day%7]
         now = datetime.datetime.now()
         forecast["requested_time"] = now.strftime("%Y-%m-%d %H:%M:%S")
+        cache.set(f'{coord["lat"]}_{coord["lon"]}_{day}', forecast)
         return forecast
